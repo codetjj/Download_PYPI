0aa75f2b988759eb1597772c4829fb98946eb9e2
Jarlan1207 <815723755@qq.com>
2021-12-16 23:00:05 +0800

    更新

--- downloadPYPI.py
+++ downloadPYPI.py
@@ -11,7 +11,8 @@ def file_name(file_dir):
     for root, dirs, files in os.walk(file_dir):
         print(root)  # 当前目录路径
         print(dirs)  # 当前路径下所有子目录
-        print(files)  # 当前路径下所有非目录子文件
+        print(len(files))  # 当前路径下所有非目录子文件
+        a = 1
         for filename in files:
             # 读取依赖包名称
             name = filename.split(".")[0]
@@ -20,14 +21,29 @@ def file_name(file_dir):
                 print("路径已存在")
             else:
                 os.mkdir("./packages/" + name)
+            print("当前进度为：",str((a/len(files))*100)+"%")
+            a = a + 1
             # 读取依赖包全部URL
             data = pd.read_csv(file_dir + filename, header=None)
+            if len(data) >=26:
+                data = data
+            else:
+                pass
             for i in range(len(data)):
                 data_name = str(data.iloc[i, 0]).split("/")[-1]
+                print(data_name)
                 # 访问URL连接，将文件保存
-                urllib.request.urlretrieve(data.iloc[i, 0], "./packages/" + name + "/" + data_name)
-                # 礼貌延时
-                sleep(1)
+                filepath = "./packages/" + name + "/" + data_name
+                if os.path.exists(filepath):
+                    print("文件已存在")
+                else:
+                    try:
+                        urllib.request.urlretrieve(data.iloc[i, 0], filepath)
+                    except:
+                        with open("testname.txt","a+") as f:
+                            f.write("{}").format(str(data.iloc[i, 0])) 
+                    # 礼貌延时
+                    sleep(2)
                 # urllib.request.urlretrieve(data.iloc[i,0])
 
 
--- getURL.py
+++ getURL.py
@@ -5,7 +5,8 @@ from time import sleep
 import os
 url = "https://pypi.tuna.tsinghua.edu.cn/simple/"
 name = "./requirements.txt"
-data = pd.read_csv(name, header=None)
+name1 = "./a.txt"
+data = pd.read_csv(name1, header=None)
 
 if os.path.exists("./packages/"):
     print("packages路径已存在")
@@ -27,9 +28,14 @@ for i in range(len(data)):
     j = []
     for a in bobj.find_all('a'):
         if "cp36" in str(a['href']) or "zip" in str(a['href']) or "py3" in str(a['href']) or "tar" in str(a['href']):
-            j.append(str(a['href']).replace("../..", "https://pypi.tuna.tsinghua.edu.cn/").split("#")[0])
+            if "macosx" in str(a['href']) or "i686" in str(a['href']) or "win32" in str(a['href']):
+                pass
+            else:
+                j.append(str(a['href']).replace("../..", "https://pypi.tuna.tsinghua.edu.cn/").split("#")[0])
     url_con = pd.DataFrame(j)
     # 将每个依赖包对应的下载连接写入同名的TXT中
     url_con.to_csv("./packages_name/" + str(data.iloc[i, 0]) + ".txt", index=None, header=None)
+    print("进度为：",i/len(data))
+    print(str(data.iloc[i, 0]))
     # 为了礼貌延时1秒
     sleep(2)
